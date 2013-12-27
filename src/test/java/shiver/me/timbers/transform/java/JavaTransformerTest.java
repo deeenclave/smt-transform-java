acc851d7ee05f25fe03a325e29027d0d3288085c
Karl Bennett <karlbennett@orcon.net.nz>
2013-12-27 19:37:55 +1300

    Set static import for TestData in JavaTransformerTest.

--- src/test/java/shiver/me/timbers/transform/java/JavaTransformerTest.java
+++ src/test/java/shiver/me/timbers/transform/java/JavaTransformerTest.java
@@ -6,6 +6,19 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
+import static shiver.me.timbers.transform.java.TestData.ALL_TRANSFORMATIONS;
+import static shiver.me.timbers.transform.java.TestData.COMMENT_TRANSFORMATIONS;
+import static shiver.me.timbers.transform.java.TestData.EMPTY_TRANSFORMATIONS;
+import static shiver.me.timbers.transform.java.TestData.INVALID_TEST_FILE_NAME;
+import static shiver.me.timbers.transform.java.TestData.KEYWORD_TRANSFORMATIONS;
+import static shiver.me.timbers.transform.java.TestData.PARENT_TRANSFORMATIONS;
+import static shiver.me.timbers.transform.java.TestData.SOURCE;
+import static shiver.me.timbers.transform.java.TestData.TRANSFORMED_COMMENTS_SOURCE;
+import static shiver.me.timbers.transform.java.TestData.TRANSFORMED_INVALID_SOURCE;
+import static shiver.me.timbers.transform.java.TestData.TRANSFORMED_KEYWORDS_SOURCE;
+import static shiver.me.timbers.transform.java.TestData.TRANSFORMED_SOURCE;
+import static shiver.me.timbers.transform.java.TestData.UNUSED_TRANSFORMATIONS;
+import static shiver.me.timbers.transform.java.TestData.readTestFile;
 
 public class JavaTransformerTest {
 
@@ -18,7 +31,7 @@ public class JavaTransformerTest {
     @Test
     public void testCreateWithParentTransformations() {
 
-        new JavaTransformer(TestData.PARENT_TRANSFORMATIONS);
+        new JavaTransformer(PARENT_TRANSFORMATIONS);
     }
 
     @Test(expected = AssertionError.class)
@@ -30,67 +43,65 @@ public class JavaTransformerTest {
     @Test
     public void testTransformation() {
 
-        assertEquals("the source should be Transformed correctly.", TestData.TRANSFORMED_SOURCE,
-                new JavaTransformer(TestData.PARENT_TRANSFORMATIONS)
-                        .transform(TestData.readTestFile(), TestData.ALL_TRANSFORMATIONS));
+        assertEquals("the source should be Transformed correctly.", TRANSFORMED_SOURCE,
+                new JavaTransformer(PARENT_TRANSFORMATIONS).transform(readTestFile(), ALL_TRANSFORMATIONS));
     }
 
     @Test
     public void testTransformationKeywordsOnly() {
 
-        assertEquals("the source should be Transformed correctly.", TestData.TRANSFORMED_KEYWORDS_SOURCE,
-                new JavaTransformer().transform(TestData.readTestFile(), TestData.KEYWORD_TRANSFORMATIONS));
+        assertEquals("the source should be Transformed correctly.", TRANSFORMED_KEYWORDS_SOURCE,
+                new JavaTransformer().transform(readTestFile(), KEYWORD_TRANSFORMATIONS));
     }
 
     @Test
     public void testTransformationCommentsOnly() {
 
-        assertEquals("the source should be Transformed correctly.", TestData.TRANSFORMED_COMMENTS_SOURCE,
-                new JavaTransformer().transform(TestData.readTestFile(), TestData.COMMENT_TRANSFORMATIONS));
+        assertEquals("the source should be Transformed correctly.", TRANSFORMED_COMMENTS_SOURCE,
+                new JavaTransformer().transform(readTestFile(), COMMENT_TRANSFORMATIONS));
     }
 
     @Test
     public void testTransformationWithInvalidSource() {
 
-        assertEquals("the source should be Transformed correctly.", TestData.TRANSFORMED_INVALID_SOURCE,
-                new JavaTransformer(TestData.PARENT_TRANSFORMATIONS)
-                        .transform(TestData.readTestFile(TestData.INVALID_TEST_FILE_NAME), TestData.ALL_TRANSFORMATIONS));
+        assertEquals("the source should be Transformed correctly.", TRANSFORMED_INVALID_SOURCE,
+                new JavaTransformer(PARENT_TRANSFORMATIONS)
+                        .transform(readTestFile(INVALID_TEST_FILE_NAME), ALL_TRANSFORMATIONS));
     }
 
     @Test
     public void testTransformationWithNoTransformations() {
 
-        assertEquals("the source should be Transformed correctly.", TestData.SOURCE,
-                new JavaTransformer().transform(TestData.readTestFile(), TestData.EMPTY_TRANSFORMATIONS)
+        assertEquals("the source should be Transformed correctly.", SOURCE,
+                new JavaTransformer().transform(readTestFile(), EMPTY_TRANSFORMATIONS)
         );
     }
 
     @Test
     public void testTransformationWithIrrelevantTransformations() {
 
-        assertEquals("the source should be Transformed correctly.", TestData.SOURCE,
-                new JavaTransformer(TestData.UNUSED_TRANSFORMATIONS)
-                        .transform(TestData.readTestFile(), TestData.UNUSED_TRANSFORMATIONS));
+        assertEquals("the source should be Transformed correctly.", SOURCE,
+                new JavaTransformer(UNUSED_TRANSFORMATIONS).transform(readTestFile(), UNUSED_TRANSFORMATIONS));
     }
 
     @Test(expected = RuntimeException.class)
     public void testTransformationWithClosedStream() throws IOException {
 
-        final InputStream closedStream = TestData.readTestFile();
+        final InputStream closedStream = readTestFile();
         closedStream.close();
 
-        new JavaTransformer().transform(closedStream, TestData.ALL_TRANSFORMATIONS);
+        new JavaTransformer().transform(closedStream, ALL_TRANSFORMATIONS);
     }
 
     @Test(expected = AssertionError.class)
     public void testTransformationWithNullTransformations() {
 
-        new JavaTransformer().transform(TestData.readTestFile(), null);
+        new JavaTransformer().transform(readTestFile(), null);
     }
 
     @Test(expected = NullPointerException.class)
     public void testTransformationWithNullInputStream() {
 
-        new JavaTransformer().transform(null, TestData.ALL_TRANSFORMATIONS);
+        new JavaTransformer().transform(null, ALL_TRANSFORMATIONS);
     }
 }
