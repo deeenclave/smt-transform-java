8f1d6019d2f66bfc89af0e8173806d54f63a4eff
Karl Bennett <karlbennett@orcon.net.nz>
2014-01-03 10:56:05 +1300

    Renamed the Transformer interface to StreamTransformer.

--- src/main/java/shiver/me/timbers/transform/java/JavaTransformer.java
+++ src/main/java/shiver/me/timbers/transform/java/JavaTransformer.java
@@ -2,15 +2,15 @@ package shiver.me.timbers.transform.java;
 
 import shiver.me.timbers.antlr4.java.JavaParser;
 import shiver.me.timbers.transform.Transformations;
-import shiver.me.timbers.transform.antlr4.Antlr4Transformer;
+import shiver.me.timbers.transform.antlr4.Antlr4StreamTransformer;
 import shiver.me.timbers.transform.antlr4.TokenTransformation;
 
 /**
- * A Transformer for Java source code, it will apply any Transformations that have names matching the different token
+ * A StreamTransformer for Java source code, it will apply any Transformations that have names matching the different token
  * and rule names. These names can be found in the {@link JavaParser#tokenNames} and {@link JavaParser#ruleNames}
  * arrays.
  */
-public class JavaTransformer extends Antlr4Transformer<JavaParser> {
+public class JavaTransformer extends Antlr4StreamTransformer<JavaParser> {
 
     public JavaTransformer() {
         super(new JavaParserBuilder());
