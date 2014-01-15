9a39b84a0746c31d5c618ebeaf24b9996dceb642
karlbennett <where_1ts@yahoo.com>
2014-01-15 20:41:38 +1300

    Updated the JavaTransformer with the MIME for Java source.

--- src/main/java/shiver/me/timbers/transform/java/JavaTransformer.java
+++ src/main/java/shiver/me/timbers/transform/java/JavaTransformer.java
@@ -3,6 +3,8 @@ package shiver.me.timbers.transform.java;
 import shiver.me.timbers.antlr4.java.JavaParser;
 import shiver.me.timbers.transform.antlr4.Antlr4StringTransformer;
 
+import javax.activation.MimeType;
+
 /**
  * A StreamTransformer for Java source code, it will apply any Transformations that have names matching the different token
  * and rule names. These names can be found in the {@link JavaParser#tokenNames} and {@link JavaParser#ruleNames}
@@ -10,7 +12,9 @@ import shiver.me.timbers.transform.antlr4.Antlr4StringTransformer;
  */
 public class JavaTransformer extends Antlr4StringTransformer<JavaParser> {
 
+    public static final MimeType TEXT_X_JAVA_SOURCE = quietMimeType("text", "x-java-source");
+
     public JavaTransformer() {
-        super(new JavaParserBuilder());
+        super(TEXT_X_JAVA_SOURCE, new JavaParserBuilder());
     }
 }
