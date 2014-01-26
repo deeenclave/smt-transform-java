06089ba33038898049aa9a33e2d5f159be9e3e0d
karlbennett <where_1ts@yahoo.com>
2014-01-26 18:25:13 +1300

    Cleaned up the code.

--- src/main/java/shiver/me/timbers/transform/java/JavaTransformer.java
+++ src/main/java/shiver/me/timbers/transform/java/JavaTransformer.java
@@ -6,8 +6,8 @@ import shiver.me.timbers.transform.antlr4.Antlr4StringTransformer;
 import javax.activation.MimeType;
 
 /**
- * A StreamTransformer for Java source code, it will apply any Transformations that have names matching the different token
- * and rule names. These names can be found in the {@link JavaParser#tokenNames} and {@link JavaParser#ruleNames}
+ * A StringTransformer for Java source code, it will apply any Transformations that have names matching the different
+ * token and rule names. These names can be found in the {@link JavaParser#tokenNames} and {@link JavaParser#ruleNames}
  * arrays.
  */
 public class JavaTransformer extends Antlr4StringTransformer<JavaParser> {
